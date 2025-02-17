1172687a3349a3ff9b99db2eb53a3f4eb0d95d0d
dreamerkumar <connectwithvkumar@gmail.com>
2025-02-17 09:27:08 -0500

    Add src/loading-effects/dots/dots.component.ts
        File type: Java source, ASCII text

--- src/loading-effects/dots/dots.component.ts
+++ src/loading-effects/dots/dots.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BackButtonComponent } from '../../app/shared/back-button.component';
 
 @Component({
   selector: 'app-dots',
   standalone: true,
+  imports: [CommonModule, BackButtonComponent],
   template: `
+    <app-back-button [text]="'Back to Loading Effects'" path="/loading-effects"></app-back-button>
     <div class="container">
       <h1>Dots Loading Effect</h1>
       <p class="description">Bouncing dots animation with staggered timing</p>
