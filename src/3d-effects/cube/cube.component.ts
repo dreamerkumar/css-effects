37f5b524bc548f95bcc7f8034e6c0b022480e306
dreamerkumar <connectwithvkumar@gmail.com>
2025-02-17 09:47:10 -0500

    Add src/3d-effects/cube/cube.component.ts
        File type: Java source, ASCII text

--- src/3d-effects/cube/cube.component.ts
+++ src/3d-effects/cube/cube.component.ts
@@ -1,11 +1,13 @@
 import { Component, ElementRef, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BackButtonComponent } from '../../app/shared/back-button.component';
 
 @Component({
   selector: 'app-cube',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BackButtonComponent],
   template: `
+    <app-back-button [text]="'Back to 3D Effects'" path="/3d-effects"></app-back-button>
     <div class="container">
       <h1>3D Cube Effect</h1>
       <p class="description">Interactive 3D cube with CSS transforms</p>
