92f79f7ca7206993a75696f8ef7551a41426ad37
dreamerkumar <connectwithvkumar@gmail.com>
2025-02-17 09:42:11 -0500

    Add src/hover-effects/scale/scale.component.ts
        File type: Java source, ASCII text

--- src/hover-effects/scale/scale.component.ts
+++ src/hover-effects/scale/scale.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BackButtonComponent } from '../../app/shared/back-button.component';
 
 @Component({
   selector: 'app-scale',
   standalone: true,
+  imports: [CommonModule, BackButtonComponent],
   template: `
+    <app-back-button [text]="'Back to Hover Effects'" path="/hover-effects"></app-back-button>
     <div class="container">
       <h1>Scale Hover Effect</h1>
       <p class="description">Elements that smoothly scale up on hover</p>
